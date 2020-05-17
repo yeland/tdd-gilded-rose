27dd88490f6d0ad6c6e7b9827b4f4561cac17c58
Linxiao <linxiao.wei@rea-group.com>
2020-05-17 22:42:47 +0800

    Add another test case for aged brie

--- src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
@@ -20,4 +20,12 @@ public class AgedBrieTest {
         assertEquals(-1, agedBrie.getSellIn());
         assertEquals(22, agedBrie.getQuantity());
     }
+
+    @Test
+    void should_get_sellIn_is_n2_quantity_is_22_when_pass_one_day_given_a_Aged_Brie_with_sellIn_is_n1_quantity_is_20() {
+        AgedBrie agedBrie = new AgedBrie(-1, 20);
+        agedBrie.passOneDay();
+        assertEquals(-2, agedBrie.getSellIn());
+        assertEquals(22, agedBrie.getQuantity());
+    }
 }
